97095b4bdae2f4a8eb82a639e030f6b5e2fb7c48
Gordon <tofugordon294@gmail.com>
2024-08-23 17:32:46 -0700

    dynamic padding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,10 +14,37 @@ config.audible_bell = "Disabled"
 config.color_scheme = "Gruvbox dark, medium (base16)"
 
 config.window_padding = {
-	left = "16pt",
-	right = "12pt",
-	top = "18pt",
-	bottom = "15pt",
+	left = "24pt",
+	right = "21pt",
+	top = "24pt",
+	bottom = "24pt",
 }
+
+-- Event handler to change padding when Neovim is the active application
+wezterm.on("update-right-status", function(window, pane)
+	local process_name = pane:get_foreground_process_name()
+
+	-- Check if the process is Neovim
+	if process_name and process_name:match("nvim") then
+		window:set_config_overrides({
+			window_padding = {
+				left = "24pt",
+				right = "21pt",
+				top = "24pt",
+				bottom = "10pt", -- Adjust the padding here for Neovim
+			},
+		})
+	else
+		-- Revert to default padding when not in Neovim
+		window:set_config_overrides({
+			window_padding = {
+				left = "24pt",
+				right = "21pt",
+				top = "24pt",
+				bottom = "24pt",
+			},
+		})
+	end
+end)
 
 return config
