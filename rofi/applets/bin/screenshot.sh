f148881637c376a1867df407d9bb8718c24c4800
Gordon <tofugordon294@gmail.com>
2024-12-18 20:10:09 -0800

    yooooo

--- rofi/applets/bin/screenshot.sh
+++ rofi/applets/bin/screenshot.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-
-## Author  : Aditya Shakya (adi1090x)
-## Github  : @adi1090x
-#
-## Applets : Screenshot
-
 # Import Current Theme
 source "$HOME"/.config/rofi/applets/shared/theme.bash
 theme="$type/$style"
@@ -37,13 +30,13 @@ if [[ "$layout" == 'NO' ]]; then
 	option_1=" Capture Desktop"
 	option_2=" Capture Area"
 	option_3=" Capture Window"
-	option_4=" Capture in 5s"
-	option_5=" Capture in 10s"
+	option_4=" Copy Area to Clipboard"
+	option_5=" Capture in 8s"
 else
 	option_1=""
 	option_2=""
 	option_3=""
-	option_4=""
+	option_4=""
 	option_5=""
 fi
 
@@ -51,7 +44,7 @@ fi
 rofi_cmd() {
 	rofi -theme-str "window {width: $win_width;}" \
 		-theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
+		-theme-str 'textbox-prompt-colon {str: " ";}' \
 		-dmenu \
 		-p "$prompt" \
 		-mesg "$mesg" \
@@ -64,102 +57,65 @@ run_rofi() {
 	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
 }
 
-# Screenshot
-time=`date +%Y-%m-%d-%H-%M-%S`
-geometry=`xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current'`
+# Screenshot directory
+time=$(date +%Y-%m-%d-%H-%M-%S)
+geometry=$(xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current')
 dir="`xdg-user-dir PICTURES`/Screenshots"
-file="Screenshot_${time}_${geometry}.png"
+file="$dir/Screenshot_${time}_${geometry}.png"
 
+# Create the Screenshots folder if it doesn't exist
 if [[ ! -d "$dir" ]]; then
 	mkdir -p "$dir"
 fi
 
-# notify and view screenshot
-notify_view() {
-	notify_cmd_shot='dunstify -u low --replace=699'
-	${notify_cmd_shot} "Copied to clipboard."
-	viewnior ${dir}/"$file"
-	if [[ -e "$dir/$file" ]]; then
-		${notify_cmd_shot} "Screenshot Saved."
-	else
-		${notify_cmd_shot} "Screenshot Deleted."
-	fi
+# Save screenshot to specified location
+save_shot () {
+	hyprshot -m output --output-folder "$dir" --filename "$(basename "$file")"
 }
 
-# Copy screenshot to clipboard
-copy_shot () {
-	tee "$file" | xclip -selection clipboard -t image/png
+# Save selected area to directory
+save_area () {
+	hyprshot -m region --output-folder "$dir" --filename "$(basename "$file")"
 }
 
-# countdown
-countdown () {
-	for sec in `seq $1 -1 1`; do
-		dunstify -t 1000 --replace=699 "Taking shot in : $sec"
-		sleep 1
-	done
+# Copy selected area to clipboard
+copy_area_clipboard () {
+	hyprshot -m region --clipboard-only
 }
 
-# take shots
+# Take shots
 shotnow () {
-	cd ${dir} && sleep 0.5 && maim -u -f png | copy_shot
-	notify_view
-}
-
-shot5 () {
-	countdown '5'
-	sleep 1 && cd ${dir} && maim -u -f png | copy_shot
-	notify_view
-}
-
-shot10 () {
-	countdown '10'
-	sleep 1 && cd ${dir} && maim -u -f png | copy_shot
-	notify_view
+	save_shot
 }
 
 shotwin () {
-	cd ${dir} && maim -u -f png -i `xdotool getactivewindow` | copy_shot
-	notify_view
+	hyprshot -m window --output-folder "$dir" --filename "$(basename "$file")"
 }
 
-shotarea () {
-	cd ${dir} && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l | copy_shot
-	notify_view
+# Capture in 8 seconds (sleep 8 and run hyprshot)
+shot8sec () {
+	sleep 8
+	hyprshot -m output --output-folder "$dir" --filename "$(basename "$file")"
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		shotnow
-	elif [[ "$1" == '--opt2' ]]; then
-		shotarea
-	elif [[ "$1" == '--opt3' ]]; then
-		shotwin
-	elif [[ "$1" == '--opt4' ]]; then
-		shot5
-	elif [[ "$1" == '--opt5' ]]; then
-		shot10
-	fi
+	case "$1" in
+		--opt1) shotnow ;;
+		--opt2) save_area ;;  # Save selected area to directory
+		--opt3) shotwin ;;
+		--opt4) copy_area_clipboard ;;  # Copy selected area to clipboard
+		--opt5) shot8sec ;;  # Capture after sleeping for 8 seconds
+	esac
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $option_1)
-		run_cmd --opt1
-        ;;
-    $option_2)
-		run_cmd --opt2
-        ;;
-    $option_3)
-		run_cmd --opt3
-        ;;
-    $option_4)
-		run_cmd --opt4
-        ;;
-    $option_5)
-		run_cmd --opt5
-        ;;
+    $option_1) run_cmd --opt1 ;;
+    $option_2) run_cmd --opt2 ;;
+    $option_3) run_cmd --opt3 ;;
+    $option_4) run_cmd --opt4 ;;
+    $option_5) run_cmd --opt5 ;;
 esac
-
 
